f3e82fd32a4321266adacde87f12b407b159d265
dssgabriel <dss.gabriel@protonmail.com>
2022-11-22 19:02:16 +0100

    Fixed linker error on template constructor
    Moved constructor implementation back to `include/csr.hpp` so that the linker does not complain when constructing a `Csr` object.

--- include/csr.hpp
+++ include/csr.hpp
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <cmath>
+#include <iostream>
 #include <vector>
 
 using size_t = std::size_t;
@@ -6,31 +9,49 @@ using size_t = std::size_t;
 template <typename T>
 class Csr {
   public:
-    Csr(std::vector<T> const& sp_mtx);
+    Csr(std::vector<T> const& sp_mtx)
+        : nb_rows(static_cast<size_t>(sqrt(sp_mtx.size())))
+    {
+        size_t nnz = 0;
+        this->row_idx.push_back(nnz);
+        for (size_t i = 0; i < this->nb_rows; ++i) {
+            for (size_t j = 0; j < this->nb_rows; ++j) {
+                if (sp_mtx[i * this->nb_rows + j] != 0.0) {
+                    this->values.push_back(sp_mtx[i * this->nb_rows + j]);
+                    this->col_idx.push_back(j);
+                    nnz++;
+                }
+            }
+            this->row_idx.push_back(nnz);
+        }
+    }
     ~Csr() = default;
 
-    std::vector<T> const& get_values() const {
-        return values;
-    };
-    std::vector<size_t> const& get_col_idx() const {
-        return col_idx;
-    };
-    std::vector<size_t> const& get_row_idx() const {
-        return row_idx;
-    };
-    size_t get_nb_rows() const {
-        return nb_rows;
-    };
+    friend std::ostream& operator<<(std::ostream& os, Csr const& csr) {
+        os << "Csr { values: [";
+        for (auto val: csr.get_values()) {
+            os << val << ", ";
+        }
+        os << "\b\b], col_idx: [";
+        for (auto val: csr.get_col_idx()) {
+            os << val << ", ";
+        }
+        os << "\b\b], row_idx: [";
+        for (auto val: csr.get_row_idx()) {
+            os << val << ", ";
+        }
+        os << "\b\b], nb_rows: " << csr.get_nb_rows() << " }";
+        return os;
+    }
 
-    std::vector<T>& get_values() {
-        return values;
-    };
-    std::vector<size_t>& get_col_idx() {
-        return col_idx;
-    };
-    std::vector<size_t>& get_row_idx() {
-        return row_idx;
-    };
+    std::vector<T> const& get_values() const { return values; };
+    std::vector<size_t> const& get_col_idx() const { return col_idx; };
+    std::vector<size_t> const& get_row_idx() const { return row_idx; };
+    size_t get_nb_rows() const { return nb_rows; };
+
+    std::vector<T>& get_values() { return values; };
+    std::vector<size_t>& get_col_idx() { return col_idx; };
+    std::vector<size_t>& get_row_idx() { return row_idx; };
 
   private:
     std::vector<T> values;
