09786450d16a8013f3b28af214dbaffcec31fc53
ZzDaBing <guillaume.bigand@ens.uvsq.fr>
2022-11-22 15:07:00 +0100

    Implemented CSR constructor in .cpp and added getters in .hpp

--- include/csr.hpp
+++ include/csr.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 
 using size_t = std::size_t;
@@ -12,4 +13,9 @@ class Csr {
         std::vector<T> values;
         std::vector<size_t> col_idx;
         std::vector<size_t> row_idx;
+
+        std::vector<T> getValues(){return values};
+        std::vector<size_t> getCol(){return col_idx};
+        std::vector<size_t> getRow(){return row_idx};
+
 };
--- src/csr.cpp
+++ src/csr.cpp
@@ -3,4 +3,18 @@
 template <typename T>
 Csr<T>::Csr(std::vector<T> const& sp_mtx) {
     // sparsify the input matrix into CSR format
+    int cmp = 0;
+    int n = sqrt(sp_mtx.size());
+
+    for(int i = 0; i < n; ++i){
+        this->row_idx[i*n] = cmp;
+        for(j = 0; i < n; ++j){
+            if(sp_mtx[n*i+j] != 0){
+                this->values[cmp] = sp_mtx[n*i+j];
+                this->col_idx[cmp] = j;
+                cmp++;
+            }
+        }
+    }
+    this->row_idx[n] = cmp;
 }
