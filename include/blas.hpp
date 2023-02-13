ee07dfd22cb66adb907f93cc90467cdab8f8785f
dssgabriel <dss.gabriel@protonmail.com>
2023-02-13 03:16:21 +0100

    Minor corrections to BLAS functions/documentation

--- include/blas.hpp
+++ include/blas.hpp
@@ -1,17 +1,57 @@
 #pragma once
 
-#include "csr.hpp"
+#include "csmat.hpp"
 
-namespace chps_blas {
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+
+namespace blas {
     /**
-     * Sparse matrix-vector multiplication for the CSR format.
+     * Computes the norm-2 of a vector.
+     *
+     * @param x An input vector.
+     * @return The norm-2 of `x`.
      **/
     template <typename T>
-    void spmv(Csr<T>& mtx, T const* x, T* y) {
-        for (size_t i = 0; i < mtx.get_nb_rows(); ++i) {
-            for (size_t j = mtx.get_row_idx()[i]; j < mtx.get_row_idx()[i + 1]; ++j) {
-                y[i] += mtx.get_values()[j] * x[mtx.get_col_idx()[j]];
+    auto norm2(std::vector<T> const& x) -> T {
+        return sqrt(std::reduce(x.begin(), x.end(), 0.0));
+    }
+
+    /**
+     * Normalizes a vector.
+     *
+     * @param x An input vector.
+     * @return The norm-2 of `x`.
+     **/
+    template <typename T>
+    auto normalize(std::vector<T>& x) -> void {
+        T norm = norm2(x);
+        std::for_each(x.begin(), x.end(), [norm](T& v) {
+            v /= norm;
+        });
+    }
+
+    /**
+     * Sparse matrix-vector multiplication for the Compressed Sparse format (CSR or CSC).
+     *
+     * @param mtx A matrix in the CS format.
+     * @param x A vector.
+     * @param y The output vector.
+     **/
+    template <typename T>
+    void spmv(CsMat<T>& mtx, T alpha, T const* x, T beta, T* y) {
+        size_t dim = mtx.get_outer_dim();
+
+        std::vector<T> const& values = mtx.get_data();
+        std::vector<size_t> const& indices = mtx.get_indices();
+        std::vector<size_t> const& indptr = mtx.get_indptr();
+
+        for (size_t i = 0; i < dim; ++i) {
+            for (size_t j = indptr[i]; j < indptr[i + 1]; ++j) {
+                y[i] += alpha * values[j] * x[indices[j]];
             }
+            y[i] *= beta;
         }
     }
-} // namespace chps_blas
+} // namespace blas
--- test/blas.cpp
+++ test/blas.cpp
@@ -1,41 +1,70 @@
 #include "blas.hpp"
-#include "csr.hpp"
+
+#include "csmat.hpp"
 
 #include <cstdint>
 #include <iostream>
 #include <vector>
 
-constexpr size_t dim = 4;
-
 auto main(int32_t argc, char** argv) -> int32_t {
-    const std::vector<double> unit_vec {1.0, 1.0, 1.0, 1.0};
-    std::vector<double> res {0.0, 0.0, 0.0, 0.0};
+    size_t ncols = 3;
+    size_t nrows = 4;
+    const std::vector<double> unit_vec(nrows, 1.0);
+    std::vector<double> res_csr(nrows, 0.0);
+    std::vector<double> res_csc(nrows, 0.0);
 
     // Example sparse matrix
     std::vector<double> sp_mtx = {
-        5.0, 0.0, 0.0, 0.0,
-        0.0, 8.0, 0.0, 0.0,
-        0.0, 0.0, 3.0, 0.0,
-        0.0, 6.0, 0.0, 0.0
+        0.0, 0.0, 8.0, 5.0, 0.0, 0.0, 0.0, 3.0, 0.0, 6.0, 0.0, 0.0,
     };
     std::cout << "sp_mtx = [" << std::endl;
-    for (size_t i = 0; i < dim; ++i) {
+    for (size_t i = 0; i < nrows; ++i) {
         std::cout << "  [";
-        for (size_t j = 0; j < dim; ++j) {
-            std::cout << sp_mtx[i * dim + j] << ", ";
+        for (size_t j = 0; j < ncols; ++j) {
+            std::cout << sp_mtx[i * ncols + j] << ", ";
         }
         std::cout << "\b\b]\n";
     }
     std::cout << "]" << std::endl;
 
-    // Sparisify the sparse matrix to a CSR format
-    Csr<double> csr_mtx = Csr(sp_mtx);
+    // Sparsify the sparse matrix to a CSR format
+    CsMat<double> csr_mtx = CsMat<double>::new_csr(ncols, nrows, sp_mtx);
     std::cout << "csr_mtx = " << csr_mtx << std::endl;
+    // Sparsify the sparse matrix to a CSC format
+    CsMat<double> csc_mtx = CsMat<double>::new_csc(ncols, nrows, sp_mtx);
+    std::cout << "csc_mtx = " << csc_mtx << std::endl;
 
     // Compute matrix-vector product using CSR format
-    chps_blas::spmv(csr_mtx, unit_vec.data(), res.data());
+    blas::spmv(csr_mtx, 1.0, unit_vec.data(), 1.0, res_csr.data());
     std::cout << "csr_mtx * unit_vec = [";
-    for (auto val: res) {
+    for (double& val : res_csr) {
+        std::cout << val << ", ";
+    }
+    std::cout << "\b\b]" << std::endl;
+    // Compute matrix-vector product using CSC format
+    blas::spmv(csc_mtx, 1.0, unit_vec.data(), 1.0, res_csc.data());
+    std::cout << "csc_mtx * unit_vec = [";
+    for (double& val : res_csc) {
+        std::cout << val << ", ";
+    }
+    std::cout << "\b\b]" << std::endl;
+
+    // Norm-2 of `res_csr`
+    std::cout << "\n|res_csr| = " << blas::norm2(res_csr) << std::endl;
+    // normalize `res`
+    blas::normalize(res_csr);
+    std::cout << "res_csr / |res_csr| = [";
+    for (double& val : res_csr) {
+        std::cout << val << ", ";
+    }
+    std::cout << "\b\b]" << std::endl;
+
+    // Norm-2 of `res_csc`
+    std::cout << "\n|res_csc| = " << blas::norm2(res_csc) << std::endl;
+    // normalize `res`
+    blas::normalize(res_csc);
+    std::cout << "res_csc / |res_csc| = [";
+    for (double& val : res_csc) {
         std::cout << val << ", ";
     }
     std::cout << "\b\b]" << std::endl;
