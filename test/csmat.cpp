2aa2e935013fa8c0297302cfdfdf6a9b45b38d55
dssgabriel <dss.gabriel@protonmail.com>
2023-02-15 17:47:51 +0100

    Replaced test matrix by an adjaceny matrix

--- test/csmat.cpp
+++ test/csmat.cpp
@@ -7,17 +7,18 @@
 
 auto main() -> int32_t {
     std::vector<double> dense_mtx = {
-        1.0, 0.0, 4.0, 0.0,
-        0.0, 2.0, 0.0, 5.0,
-        0.0, 0.0, 3.0, 6.0,
+        1.0, 0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0, 0.0,
     };
 
     std::cout << "CSR matrix:\n  ";
-    CsMat<double> csr_mtx = CsMat<double>::new_csr(4, 3, dense_mtx);
+    CsMat<double> csr_mtx = CsMat<double>::new_csr(4, 4, dense_mtx);
     std::cout << csr_mtx << "\n";
 
     std::cout << "CSC matrix:\n  ";
-    CsMat<double> csc_mtx = CsMat<double>::new_csc(4, 3, dense_mtx);
+    CsMat<double> csc_mtx = CsMat<double>::new_csc(4, 4, dense_mtx);
     std::cout << csc_mtx << "\n";
 
     std::cout << "\nTransposed CSR matrix (equals CSC):\n  ";
